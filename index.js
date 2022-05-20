fb4e51ae64228464e0e4d2ffe5e1833c628b493d
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-05-19 19:42:47 -0500

    commit tree 254ec7f3a044befbbb6b8bec4a5ffb4321a8ae5a

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockDist) {
+    if (blockDist > 42) {
+        return blockDist - 42
+    } else if (blockDist < 42) {
+        return 42 - blockDist
+    }
+}
+
+function distanceFromHqInFeet(feetDist) {
+    distanceFromHqInBlocks(feetDist);
+    if (feetDist > 42) {
+        return (feetDist - 42) * 264
+    } else if (feetDist < 42) {
+        return (42 - feetDist) * 264
+    }
+}
+
+function distanceTravelledInFeet(travelDis1, travelDis2) {
+    if (travelDis2 > travelDis1) {
+        return (travelDis2 - travelDis1) * 264
+    } else if (travelDis2 < travelDis1) {
+        return (travelDis1 - travelDis2) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(destination, start);
+    
+    if (distance < 400) {
+        return 0;
+    }   else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02
+    } else if (distance > 2000 && distance <2500) {
+        return 25;
+    } else {
+        return "cannot travel that far"
+    }
+        
+}
